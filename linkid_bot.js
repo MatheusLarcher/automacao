9117005a61a819a53c80e584e24fb50e8cc14018
Matheus <matheuslarcher20@gmail.com>
2024-03-06 08:18:21 -0300

    bot

--- linkid_bot.js
+++ linkid_bot.js
@@ -9,17 +9,39 @@ async function run() {
     const page = await browser.newPage(); // Abre uma nova página
     await page.goto('https://www.linkedin.com/jobs/search/?currentJobId=3839343017&f_AL=true&keywords=python&origin=JOB_SEARCH_PAGE_JOB_FILTER&sortBy=R');
 
-    //Nao encontra a div
-    await page.waitForSelector('.ember-view.jobs-search-results__list-item');
+    await waitFor(2);
 
+    await page.waitForSelector('.ember-view.jobs-search-results__list-item');
     await page.click('.ember-view.jobs-search-results__list-item');
 
-    await page.waitForSelector('.jobs-apply-button');
+    await waitFor(1);
 
-    // Clica no botão
+    await page.waitForSelector('.jobs-apply-button');
     await page.click('.jobs-apply-button');
 
+    //não ta pegando as divs certas
+    var v = getEditableElements(page)
 }
 
+function waitFor(seconds) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), seconds * 1000);
+    });
+}
+
+async function getEditableElements(page) {
+    await waitFor(5);
+    await page.waitForSelector('.artdeco-modal__content.jobs-easy-apply-modal__content.p0.ember-view');
+
+    const elements = await page.$$('input, textarea');
+
+    const editableElements = await Promise.all(elements.map(async (element) => {
+        const name = await page.evaluate(el => el.getAttribute('name'), element);
+        const type = await page.evaluate(el => el.getAttribute('type'), element);
+        return { name, type };
+    }));
+
+    return editableElements;
+}
 
 run();
