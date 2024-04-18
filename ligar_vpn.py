9f4f68d1f749e725c8d6294f05b4b876e5fd12e5
Matheus <matheuslarcher20@gmail.com>
2024-04-18 11:08:12 -0300

    Create ligar_vpn.py

---
+++ ligar_vpn.py
@@ -0,0 +1,29 @@
+from subprocess import Popen
+from pywinauto import Desktop
+
+app = Popen("C:\Program Files\OpenVPN Connect\OpenVPNConnect.exe", shell = True)
+
+
+dialog = Desktop(backend='uia').window(title='OpenVPN Connect', found_index=0)
+dialog.wait('visible')
+
+        
+def conectar_vpn(nome_vpn):    
+    vpn_button = dialog.child_window(title="widmen", control_type="Group").wrapper_object()
+    vpn_button.invoke()
+    
+def desconectar_vpn(nome_vpn):
+    # Navigate to the specific group control and invoke it
+    disconnect_button = dialog.child_window(title="Disconnect profile vpn-widmen.ccmcloud.com.br [Widmen]", control_type="Group")
+    disconnect_button.wait('visible', timeout=10)
+    
+    # Since the control supports InvokePattern, we can use invoke() method
+    disconnect_button.invoke()
+    
+    
+conectar_vpn('empresa')
+
+import time 
+time.sleep(10)
+desconectar_vpn('empresa')
+
